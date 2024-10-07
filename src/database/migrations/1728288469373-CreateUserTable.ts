0d719fc6a6e361e65363f6fd76aecb035490d633
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2024-10-07 22:20:58 +0700

    Add CreateUserTable migration

---
+++ src/database/migrations/1728288469373-CreateUserTable.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTable1728288469373 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'User',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            length: '10',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '20',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '50',
+          },
+          {
+            name: 'role',
+            type: 'varchar',
+            length: '20',
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('User');
+  }
+}
