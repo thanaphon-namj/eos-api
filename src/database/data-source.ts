7c070bb0d155b155aecc390354e29f107bcf7860
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2024-10-07 22:20:20 +0700

    Add typeorm data source

---
+++ src/database/data-source.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: [__dirname + '/migrations/*.ts'],
+});
+
+export default dataSource;
