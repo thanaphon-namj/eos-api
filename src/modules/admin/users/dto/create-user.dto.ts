f75b1f397e6e2ab9ab82c2430a5ec37516f5213c
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2024-11-06 16:22:04 +0700

    Add user module

---
+++ src/modules/admin/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  email: string;
+}
