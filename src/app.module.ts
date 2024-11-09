8699548478bf35effa8b1b4efe7347b6ce241e6b
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2024-11-10 03:44:22 +0700

    commit for test deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './modules/admin/users/user.entity';
-import { AdminModule } from './modules/admin/admin.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './modules/admin/users/user.entity';
+// import { AdminModule } from './modules/admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DB } from './environments';
+// import { DB } from './environments';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: DB.HOST,
-      port: DB.PORT,
-      username: DB.USERNAME,
-      password: DB.PASSWORD,
-      database: DB.NAME,
-      entities: [User],
-    }),
-    AdminModule,
+    // ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: DB.HOST,
+    //   port: DB.PORT,
+    //   username: DB.USERNAME,
+    //   password: DB.PASSWORD,
+    //   database: DB.NAME,
+    //   entities: [User],
+    // }),
+    // AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
