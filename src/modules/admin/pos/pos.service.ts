14f8fd84d7ceebd4fda9d6541e6f70910b88c87f
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2024-11-17 21:31:42 +0700

    Add get order inbox in admin pos

---
+++ src/modules/admin/pos/pos.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { OrdersService } from '../../orders/orders.service';
+import { OrderStatus } from '../../orders/order.entity';
+
+@Injectable()
+export class AdminPosService {
+  constructor(private ordersService: OrdersService) {}
+
+  getInbox() {
+    return this.ordersService.findAllByStatus(OrderStatus.Confirmed);
+  }
+}
