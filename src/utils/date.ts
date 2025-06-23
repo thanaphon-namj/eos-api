248d19b8c681a8848f250a377eac03842429a0fb
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2025-06-24 02:09:08 +0700

    commit

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,24 +1,30 @@
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(isSameOrAfter);
 
+dayjs.tz.setDefault('Asia/Bangkok');
+
 export const now = () => {
-  return dayjs().format('YYYY-MM-DD HH:mm:ss');
+  return dayjs.tz().format('YYYY-MM-DD HH:mm:ss');
 };
 
 export const getStartOfDay = () => {
-  return dayjs().startOf('day').format('YYYY-MM-DD HH:mm:ss');
+  return dayjs.tz().startOf('day').format('YYYY-MM-DD HH:mm:ss');
 };
 
 export const getEndOfDay = () => {
-  return dayjs().endOf('day').format('YYYY-MM-DD HH:mm:ss');
+  return dayjs.tz().endOf('day').format('YYYY-MM-DD HH:mm:ss');
 };
 
 export const addMinutes = (date: Date | string, minutes: number) => {
-  return dayjs(date).add(minutes, 'minute').format('YYYY-MM-DD HH:mm:ss');
+  return dayjs.tz(date).add(minutes, 'minute').format('YYYY-MM-DD HH:mm:ss');
 };
 
 export const isMoreThanOrEqual = (date: Date | string) => {
-  return dayjs().isSameOrAfter(dayjs(date));
+  return dayjs.tz().isSameOrAfter(dayjs.tz(date));
 };
