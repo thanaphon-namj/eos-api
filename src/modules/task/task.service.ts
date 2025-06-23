8af8d0f53f28842f4de86af86ac9240f577c5b1d
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2025-06-23 23:18:00 +0700

    commit

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -18,8 +18,8 @@ export class TaskService {
     private settingService: SettingService,
   ) {}
 
-  @Cron('*/2 * * * *')
-  // @Cron('*/5 8-20 * * *')
+  @Cron('*/3 * * * *')
+  // @Cron('*/3 8-20 * * *')
   async handleCron() {
     const schedules = await this.scheduleRepository.find({
       where: {
